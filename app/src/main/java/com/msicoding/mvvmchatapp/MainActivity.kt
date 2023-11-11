46d2dbbe8eb91482e56d0c5b2f523e11e6ca0e35
msiprime <msisakib958@gmail.com>
2023-11-11 23:48:26 +0600

    checking

--- app/src/main/java/com/msicoding/mvvmchatapp/MainActivity.kt
+++ app/src/main/java/com/msicoding/mvvmchatapp/MainActivity.kt
@@ -3,13 +3,19 @@ package com.msicoding.mvvmchatapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.msicoding.mvvmchatapp.ui.theme.MVVMchatAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +26,8 @@ class MainActivity : ComponentActivity() {
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+//                    color = MaterialTheme.colorScheme.background
+                    color = Color.Red
                 ) {
                     Greeting("Android")
                 }
@@ -31,10 +38,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        modifier = Modifier.background(Color.Magenta)
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+                .padding(10.dp)
+                .background(Color.Cyan)
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(10.dp)
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
